e7357b0a62e4fac64ff73ee9604e9f0ddc77a9ec
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2024-12-13 17:03:39 -0800

    feat: add tooltip to sidebar

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -3,11 +3,9 @@
 	import HomeIcon from './icons/HomeIcon.svelte';
 	import PreferencesIcon from './icons/PreferencesIcon.svelte';
 	import WatchlistIcon from './icons/WatchlistIcon.svelte';
+	import Tooltip from './ui/tooltip/Tooltip.svelte';
 
-	// Uses link number to calculate highlighting div position
-	// in 40px increments
 	let currentLink = $state(1);
-	const translateAmount = $derived((currentLink - 1) * 12);
 
 	function updateCurrentLink(linkNum: number) {
 		currentLink = linkNum;
@@ -21,51 +19,63 @@
 		<div class="flex w-full flex-col items-center justify-between text-white/60">
 			<!-- Active-Link Highlighter -->
 			<div
-				class={`ease absolute right-0 h-10 w-[1px] origin-top translate-y-${translateAmount} bg-blue-400 transition-transform delay-75 duration-200`}
+				class:translate-y-0={currentLink === 1}
+				class:translate-y-12={currentLink === 2}
+				class:translate-y-24={currentLink === 3}
+				class:translate-y-36={currentLink === 4}
+				class={`ease absolute right-0 h-10 w-[1px] origin-top bg-blue-400 transition-transform delay-75 duration-200`}
 			></div>
 
-			<span class="mb-2">
-				<a
-					href="/"
-					onclick={() => updateCurrentLink(1)}
-					class:text-blue-400={currentLink === 1}
-					class="before:hover:bg-black-500 relative z-10 flex items-center justify-center transition-all duration-200 ease-out before:absolute before:-z-10 before:mx-auto before:size-8 before:rounded before:opacity-0 before:transition-all before:duration-200 before:ease-out hover:text-blue-400 before:hover:opacity-35"
-				>
-					<HomeIcon classes="w-5 h-10" />
-				</a>
+			<span class="mb-0.5">
+				<Tooltip content="Home">
+					<a
+						href="/"
+						onclick={() => updateCurrentLink(1)}
+						class:text-blue-400={currentLink === 1}
+						class="before:hover:bg-black-500 relative z-10 flex items-center justify-center transition-all duration-200 ease-out before:absolute before:-z-10 before:mx-auto before:size-8 before:rounded before:opacity-0 before:transition-all before:duration-200 before:ease-out hover:text-blue-400 before:hover:opacity-35"
+					>
+						<HomeIcon classes="w-5 h-10" />
+					</a>
+				</Tooltip>
 			</span>
 
-			<span class="mb-2">
-				<a
-					href="/"
-					onclick={() => updateCurrentLink(2)}
-					class:text-blue-400={currentLink === 2}
-					class="before:hover:bg-black-500 relative z-10 flex items-center justify-center transition-all duration-200 ease-out before:absolute before:-z-10 before:mx-auto before:size-8 before:rounded before:opacity-0 before:transition-all before:duration-200 before:ease-out hover:text-blue-400 before:hover:opacity-35"
-				>
-					<MetricsIcon classes="w-5 h-10" />
-				</a>
+			<span class="mb-0.5">
+				<Tooltip content="Metrics">
+					<a
+						href="/"
+						onclick={() => updateCurrentLink(2)}
+						class:text-blue-400={currentLink === 2}
+						class="before:hover:bg-black-500 relative z-10 flex items-center justify-center transition-all duration-200 ease-out before:absolute before:-z-10 before:mx-auto before:size-8 before:rounded before:opacity-0 before:transition-all before:duration-200 before:ease-out hover:text-blue-400 before:hover:opacity-35"
+					>
+						<MetricsIcon classes="w-5 h-10" />
+					</a>
+				</Tooltip>
 			</span>
 
-			<span class="mb-2">
-				<a
-					href="/"
-					onclick={() => updateCurrentLink(3)}
-					class:text-blue-400={currentLink === 3}
-					class="before:hover:bg-black-500 relative z-10 flex items-center justify-center transition-all duration-200 ease-out before:absolute before:-z-10 before:mx-auto before:size-8 before:rounded before:opacity-0 before:transition-all before:duration-200 before:ease-out hover:text-blue-400 before:hover:opacity-35"
-				>
-					<WatchlistIcon classes="w-5 h-10" />
-				</a>
+			<span class="mb-0.5">
+				<Tooltip content="Watchlist">
+					<a
+						href="/"
+						onclick={() => updateCurrentLink(3)}
+						class:text-blue-400={currentLink === 3}
+						class="before:hover:bg-black-500 relative z-10 flex items-center justify-center transition-all duration-200 ease-out before:absolute before:-z-10 before:mx-auto before:size-8 before:rounded before:opacity-0 before:transition-all before:duration-200 before:ease-out hover:text-blue-400 before:hover:opacity-35"
+					>
+						<WatchlistIcon classes="w-5 h-10" />
+					</a>
+				</Tooltip>
 			</span>
 
-			<span class="mb-2">
-				<a
-					href="/"
-					onclick={() => updateCurrentLink(4)}
-					class:text-blue-400={currentLink === 4}
-					class="before:hover:bg-black-500 ease before:ease relative z-10 flex items-center justify-center transition-all duration-200 before:absolute before:-z-10 before:mx-auto before:size-8 before:rounded before:opacity-0 before:transition-all before:duration-200 hover:text-blue-400 before:hover:opacity-35"
-				>
-					<PreferencesIcon classes="w-5 h-10" />
-				</a>
+			<span class="mb-0.5">
+				<Tooltip content="Preferences">
+					<a
+						href="/"
+						onclick={() => updateCurrentLink(4)}
+						class:text-blue-400={currentLink === 4}
+						class="before:hover:bg-black-500 ease before:ease relative z-10 flex items-center justify-center transition-all duration-200 before:absolute before:-z-10 before:mx-auto before:size-8 before:rounded before:opacity-0 before:transition-all before:duration-200 hover:text-blue-400 before:hover:opacity-35"
+					>
+						<PreferencesIcon classes="w-5 h-10" />
+					</a>
+				</Tooltip>
 			</span>
 		</div>
 	</div>
---
+++ src/lib/components/ui/tooltip/Tooltip.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import * as Tooltip from './index';
+
+	let { content, children } = $props();
+</script>
+
+<Tooltip.Provider>
+	<Tooltip.Root delayDuration={250}>
+		<Tooltip.Trigger>
+			{@render children?.()}
+		</Tooltip.Trigger>
+		<Tooltip.Content side="right" sideOffset={25}>
+			<p>{content}</p>
+		</Tooltip.Content>
+	</Tooltip.Root>
+</Tooltip.Provider>
